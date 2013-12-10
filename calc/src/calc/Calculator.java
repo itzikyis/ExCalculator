fe5c9b0471999806a20eae26dce21e584e135c30
itzikyis <itzik.yis@gmail.com>
2013-12-10 16:31:06 +0200

    REFACTOR

--- calc/src/calc/Calculator.java
+++ calc/src/calc/Calculator.java
@@ -16,8 +16,13 @@ public class Calculator
 		}
 		else
 		{
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			return getSum(numbers[0],numbers[1]);
 		}
+	}
+	
+	private int getSum(String numA, String numB)
+	{
+		return Integer.parseInt(numA) + Integer.parseInt(numB);
 	}
 	
 	private boolean isEmpty(String input)
